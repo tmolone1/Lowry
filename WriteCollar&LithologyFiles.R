c955a6e408ac43546089e514ee441ef1dbef9a4c
Tim Moloney <tmoloney@trihydro.com>
2020-06-18 11:23:45 -0600

    revisions to collar and lithology files to deal with duplicate surveys, and conflicting hole IDs in collar and lithology tables.

--- WriteCollar&LithologyFiles.R
+++ WriteCollar&LithologyFiles.R
@@ -2,11 +2,44 @@ rm(list-ls())
 library(dplyr)
 library(readr)
 library(readxl)
+library(sp)
+library(rgdal)
 list.files()
 compiled<-read_excel("Survey Data.xlsx",sheet="COMPILED")
-lith<-read_excel("Copy of LeapfrogInput-MK (3).xlsx", sheet= "Lithology")
-write_csv(compiled, "collar.csv")
-lith<-lith[,c(1:6)]
-colnames(lith)[5]<-"2nd_lithology"
-colnames(lith)[6]<-"comment"
+lith<-read_excel("Copy of LeapfrogInput-MK (3) _ TM edit.xlsx", sheet= "Lithology")
+
+#define mode function
+Mode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+
+#summarise each character variable as the mode, most frequently ocurring value
+chars<-dplyr::select_if(compiled, is.character)
+chars<- chars %>%
+  group_by(LOCATION) %>%
+  summarize_each(funs(Mode))
+
+#summarize each numeric varialbe as the mean of all records for that location
+#this is instead of taking most recent year surveyed.  
+# some points have multiple records from same year, e.g. BG-MW01, has separate records GRND, TOC, and PVC survey all in 2005
+# XYs for each are subtlely different.  How do we decide which XY to use?  
+# if we take mean of everything it doesnt matter.
+# if client wants this treated differently, should provide revised coordinate file.
+nums<-dplyr::select_if(compiled, is.numeric)
+nums<-as_tibble(cbind(compiled$LOCATION,nums))
+colnames(nums)[1]<-"LOCATION"
+nums<- nums %>%
+  group_by(LOCATION) %>%
+  summarize_each(funs(mean))
+
+collar<-merge(chars, nums, by="LOCATION")
+
+
+write_csv(collar, "collar.csv")
 write_csv(lith,"Lithology.csv")
+Wells_points <- SpatialPointsDataFrame(collar[,c(12,11)], 
+                                       data= collar,
+                                       proj4string = CRS("+init=ESRI:102654")) # colorado state plane central NAD83
+## use this to write a shapefile of the wells if desired:
+#writeOGR(Wells_points, dsn="M:/ItoN/MidnightSunTrinityJV/Modeling/Shapefiles", layer = "Wells_points", driver = "ESRI Shapefile", overwrite_layer=TRUE)
