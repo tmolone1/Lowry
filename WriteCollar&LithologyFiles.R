8bf21b937d7e21a03e7cd2b8c35926ce469e494d
Tim Moloney <tmoloney@trihydro.com>
2020-07-08 11:57:27 -0600

    Edited files to add lithology from 1950s deep soil borings.  All lithology intervals below 6 ft bgs were reclassified to consolidated lithology types.

--- WriteCollar&LithologyFiles.R
+++ WriteCollar&LithologyFiles.R
@@ -7,6 +7,23 @@ library(rgdal)
 list.files()
 compiled<-read_excel("Survey Data.xlsx",sheet="COMPILED")
 lith<-read_excel("Copy of LeapfrogInput-MK (3) _ TM edit.xlsx", sheet= "Lithology")
+deep_coords<-read_csv("1950s_Deep_Borings_Coordinates.csv")
+deep_lith<-read_csv("1950s_Deep_Borings_Lithology.csv")
+deep_lith<-deep_lith[,which(colnames(deep_lith) %in% colnames(lith))]
+lith<-merge(lith,deep_lith, by=colnames(lith), all.x=TRUE, all.y=TRUE)
+
+#Fix consolidated lithology bullcrap
+v<-unique(lith[lith$From>6,]$Valid_Lith)
+v[c(1,3)] <- "Sandstone"  # check this and edit it if necessary based on geology
+v[c(2,8,10,12,14,16,17)] <- "Shale"  # check this and edit it if necessary based on geology
+v[c(11)] <- "Clayey Sandstone"  # check this and edit it if necessary based on geology
+v[c(13)] <- "Sandy Siltstone"  # check this and edit it if necessary based on geology
+v[c(15,18)] <- "Sandy Claystone"  # check this and edit it if necessary based on geology
+df<-data.frame(cbind(unique(lith[lith$From>6,]$Valid_Lith),v))
+for (i in seq(1,nrow(df))) {
+lith[lith$From>6 & lith$Valid_Lith==df$V1[i],]$Valid_Lith<-as.character(df$v[i])
+}
+  
 
 #define mode function
 Mode <- function(x) {
@@ -34,11 +51,14 @@ nums<- nums %>%
   summarize_each(funs(mean))
 
 collar<-merge(chars, nums, by="LOCATION")
+deep_coords<-deep_coords[,3:5]
+collar<-merge(collar,deep_coords,by.x=c(1,11,12),by.y=c(1,3,2),all.x=TRUE,all.y=TRUE)
 
 #create table to identify unique lithologies and assign which are mapped to coarse in fine_coarse variable
 v<-rep("Fine",length(unique(lith$Valid_Lith)))
-v[c(2,5,7)] <- "Coarse"  # check this and edit it if necessary based on geology
+v[c(2,3,7,11)]<- "Coarse"  # check this and edit it if necessary based on geology
 df<-data.frame(cbind(unique(lith$Valid_Lith),v))
+
 lith$fine_coarse <- "Fine"
 lith[lith$Valid_Lith %in% df$V1[df$v=="Coarse"],"fine_coarse"] <- "Coarse"
 
@@ -54,8 +74,8 @@ lith_mod <- arrange(lith_mod, desc(lith_mod$`Hole ID`))
 write_csv(collar, "collar.csv")
 write_csv(lith,"Lithology.csv")
 write_csv(lith_mod,"Lithology_hi_res.csv")
-Wells_points <- SpatialPointsDataFrame(collar[,c(12,11)], 
+Wells_points <- SpatialPointsDataFrame(collar[,c(3,2)], 
                                        data= collar,
                                        proj4string = CRS("+init=ESRI:102654")) # colorado state plane central NAD83
-# # use this to write a shapefile of the wells if desired:
-# writeOGR(Wells_points, dsn="M:/ItoN/MidnightSunTrinityJV/Modeling/Shapefiles", layer = "Wells_points", driver = "ESRI Shapefile", overwrite_layer=TRUE)
+# use this to write a shapefile of the wells if desired:
+writeOGR(Wells_points, dsn="M:/ItoN/MidnightSunTrinityJV/Modeling/Shapefiles", layer = "Wells_points", driver = "ESRI Shapefile", overwrite_layer=TRUE)
