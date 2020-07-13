f4880505ff73d29b80bb1e0546e91fb2fb16246f
Tim Moloney <tmoloney@trihydro.com>
2020-07-13 12:43:29 -0600

    added code for water level data processing

---
+++ WaterLevel_Data_Processing.R
@@ -0,0 +1,66 @@
+library(sp)
+library(raster)
+library(rgdal)
+library(readr)
+library(readxl)
+library(gstat)
+collar<-read_csv("collar.csv") # from output of "WriteCollar&LithologyFiles.R"
+water_levels<-read_excel("L1B Water Levels.1.xlsx", col_types=c("text", "date", "numeric", "text", "numeric"))
+water_levels[water_levels$LOCID=='BGMW-01',]$LOCID<-"BG-MW01"  # fix mismatched ID typo
+
+# create SPDF from collar file
+Wells_points <- SpatialPointsDataFrame(collar[,c(3,2)], 
+                                       data= collar,
+                                       proj4string = CRS("+init=ESRI:102654")) # colorado state plane central NAD83
+
+plot(Wells_points) # displays all wells
+points(Wells_points[Wells_points$LOCATION %in% water_levels$LOCID,],pch=15)# displays the points with water level data
+
+# merge XY coordinates from collar file to water levels sheet
+water_levels<-merge(collar[,c(1:3)],water_levels,by.x=1,by.y=1,all.y=TRUE)
+
+# create SPDF from water level data and write shapefile
+WL_pts <- SpatialPointsDataFrame(water_levels[,c(3,2)], 
+                                       data= water_levels,
+                                       proj4string = CRS("+init=ESRI:102654")) # colorado state plane central NAD83
+writeOGR(WL_pts, dsn=getwd(), layer = "April2020_WaterLevels", driver = "ESRI Shapefile", overwrite_layer=TRUE)
+###USE to write directly to M drive
+##writeOGR(WL_pts, dsn="M:/ItoN/MidnightSunTrinityJV/Modeling/Shapefiles", layer = "April2020_WaterLevels", driver = "ESRI Shapefile", overwrite_layer=TRUE)
+
+# manually exclude locations from interpolation
+suppress<-c("IP-37") # list IDs to locations to be excluded
+# remove locations with NA data and suppressed locations from the water level data sheet
+WL_pts_int<-WL_pts[!is.na(WL_pts$`GW ELEV`) & !(WL_pts$LOCATION %in% suppress),]
+
+# setup a grid for interpolations
+grd <- as.data.frame(spsample(Wells_points, "regular", n=50000))
+names(grd)       <- c("X", "Y")
+coordinates(grd) <- c("X", "Y")
+gridded(grd)     <- TRUE  # Create SpatialPixel object
+fullgrid(grd)    <- TRUE  # Create SpatialGrid object
+# Add Wells_points's projection information to the empty grid
+proj4string(grd) <- proj4string(Wells_points)
+
+#Fit variogram and display plot to inspect goodness of fit
+v.fit2<-fit.variogram(variogram(WL_pts_int$`GW ELEV`~1, WL_pts_int), vgm(psill = 4000, "Exp", range = 7000, nugget = 0), 
+                      fit.sills = TRUE, fit.ranges = FALSE, fit.method=7)
+plot(variogram(WL_pts_int$`GW ELEV`~1, WL_pts_int), model = v.fit2)
+
+# ordinary kriging
+x <- krige(WL_pts_int$`GW ELEV`~1, locations = WL_pts_int, newdata = grd, model = v.fit2)
+spplot(x["var1.pred"], main = "ordinary kriging predictions")
+
+# display kriging output
+krig1<-raster(x) 
+plot(krig1)
+contour(krig1, levels=as.numeric(seq(6000,6070,5)), add=TRUE)
+points(WL_pts_int)
+
+# contour at specified interval and write to line shapefile
+cl<-rasterToContour(krig1, levels=as.numeric(seq(6000,6070,5))) # 5 ft contour interval
+cl$level<-as.integer(levels(cl$level)) # required to make the 'level' filed numeric when the shapefile is exported
+writeOGR(cl, dsn = getwd(), layer = "April2020_PotsurfaceContours", driver = "ESRI Shapefile", overwrite_layer=TRUE)
+# ##Use to write directly to M drive
+# writeOGR(cl, dsn="M:/ItoN/MidnightSunTrinityJV/Modeling/Shapefiles", layer = "April2020_PotsurfaceContours", driver = "ESRI Shapefile", overwrite_layer=TRUE)
+# writeOGR(cl, dsn="M:/ItoN/MidnightSunTrinityJV/Modeling/Shapefiles", layer = "April2020_PotsurfaceContours", driver="MapInfo File",
+#         dataset_options="FORMAT=MIF")
